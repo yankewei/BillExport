c695ef92eb09dd88452febdde18d88a3a6987c3d
yankewei <y947102435@gmail.com>
2025-01-01 12:40:55 +0800

    Fix the password keyborad show up, as the keyword num is changed when tapping the password input

--- main.ts
+++ main.ts
@@ -12,23 +12,31 @@ type keyboard = {
 const sleep = (ms: number): Promise<void> =>
   new Promise((r) => setTimeout(r, ms));
 
-const iPhone = devices["iPhone 14 Pro Max"];
+const iPhone = devices["iPhone 15 Pro"];
 const browser = await chromium.launch({ headless: false });
 const context = await browser.newContext({
   ...iPhone,
 });
 const page = await context.newPage();
 
-const currentPeriodBillLocator = await getCurrentPeriodBillLocator();
+let locator: Locator | null = null;
 
-console.log(currentPeriodBillLocator);
+if (flags.debug) {
+  // Load the HTML file from local, then we will do not need to actually login every time
+  const filePath = `${Deno.cwd()}/after_bill_list.html`;
+  const text = await Deno.readTextFile(filePath);
+  await page.setContent(text);
+  locator = page.locator(
+    ".my-bill-wrapper-view.flex-column",
+  ).nth(1);
+} else {
+  locator = await getCurrentPeriodBillLocator();
+}
 
 // Get All bill locator
-const billsLocator = await page.locator(
+const billsLocator = await locator.locator(
   ".common-bills-view .bills-box-view .more-bill-list-box > *",
 ).all();
-
-console.log(billsLocator);
 
 interface BillDetail {
   billName: string;
@@ -73,89 +81,91 @@ for (const billLocator of billsLocator) {
   }
 }
 
-console.log(billListByDate);
+console.log(JSON.stringify(billListByDate));
 
 // Just wait for a while, then we can see the page
 await sleep(1000000);
 Deno.exit(0);
 
 async function getCurrentPeriodBillLocator(): Promise<Locator> {
-  if (flags.debug) {
-    // Load the HTML file from local, then we will do not need to actually login every time
-    const filePath = `${Deno.cwd()}/bill_home.html`;
+  await page.goto("https://xyk.cmbchina.com/portal-express/login");
 
-    const text = await Deno.readTextFile(filePath);
-    await page.setContent(text);
-  } else {
-    await page.goto("https://xyk.cmbchina.com/portal-express/login");
+  const idOrCreditCardLocator = await page.getByPlaceholder(
+    "请输入身份证号 / 信用卡号",
+  );
 
-    const idOrCreditCardLocator = await page.getByPlaceholder(
-      "请输入身份证号 / 信用卡号",
-    );
+  const idOrCreditCardNumber = prompt("请输入身份证号 / 信用卡号:");
 
-    const idOrCreditCardNumber = prompt("请输入身份证号 / 信用卡号:");
-
-    if (idOrCreditCardNumber === null) {
-      throw new Error("Cannot get your ID or CreditCard number");
-    }
-
-    await idOrCreditCardLocator.fill(idOrCreditCardNumber);
-
-    // Pop the keyboard
-    await page.getByText("请输入查询密码").tap();
-
-    // Get the element by the keyboard num
-    const keyboardElements = await page.locator('div[data-type="num"]').all();
-    const keyboardArr: keyboard = {};
-
-    for (const keyboardElement of keyboardElements) {
-      const text = await keyboardElement.textContent();
-      if (text !== null) {
-        keyboardArr[text] = keyboardElement;
-      }
-    }
-
-    const password = prompt("请输入查询密码:");
-
-    if (password === null) {
-      throw new Error("Cannot get your password");
-    }
-
-    [...password].forEach((char) => {
-      if (keyboardArr[char] !== null) {
-        keyboardArr[char].tap();
-      }
-    });
-
-    await page.getByText("同意并登录").tap();
-
-    const verifyCode = prompt("请输入验证码:");
-
-    if (verifyCode === null) {
-      throw new Error("Cannot get your verify code");
-    }
-
-    await page.getByPlaceholder("请输入验证码").fill(`${verifyCode}`);
-
-    await page.getByText("提交").tap();
-
-    await page.goto(
-      "https://xyk.cmbchina.com/BillServiceWeb/index.shtml#/billHomePage",
-    );
-
-    const billListElements = await page.locator(
-      ".my-bill-wrapper-view.flex-column",
-    ).all();
-    const pageContent = await page.content();
-    await Deno.writeTextFile("bill_list.html", pageContent);
-
-    // List the bill for '本期账单'
-    await billListElements[1].tap();
+  if (idOrCreditCardNumber === null) {
+    throw new Error("Cannot get your ID or CreditCard number");
   }
 
-  return await page.locator(".my-bill-wrapper-view.flex-column").all().then(
-    (data) => {
-      return data[1];
-    },
+  await idOrCreditCardLocator.fill(idOrCreditCardNumber);
+
+  // Pop the keyboard
+  await page.getByText("请输入查询密码").tap();
+
+  // Wait for the keyboard to show, otherwise we cannot get the keyboard element
+  await sleep(2000);
+
+  // Get the element by the keyboard num
+  const keyboardElements = await page.locator('div[data-type="num"]').all();
+  const keyboardArr: keyboard = {};
+
+  for (const keyboardElement of keyboardElements) {
+    const text = await keyboardElement.textContent();
+    console.log(text, keyboardElement);
+    if (text !== null) {
+      keyboardArr[text] = keyboardElement;
+    }
+  }
+
+  const password = prompt("请输入查询密码:");
+
+  if (password === null) {
+    throw new Error("Cannot get your password");
+  }
+
+  for (const char of password) {
+    if (keyboardArr[char] !== null) {
+      console.log(keyboardArr[char]);
+
+      await keyboardArr[char].tap();
+    }
+  }
+
+  await page.getByText("同意并登录").tap();
+
+  const verifyCode = prompt("请输入验证码:");
+
+  if (verifyCode === null) {
+    throw new Error("Cannot get your verify code");
+  }
+
+  await page.getByPlaceholder("请输入验证码").fill(`${verifyCode}`);
+
+  await page.getByText("提交").tap();
+
+  await page.goto(
+    "https://xyk.cmbchina.com/BillServiceWeb/index.shtml#/billHomePage",
   );
+
+  // Wait the page loaded
+  await sleep(5000);
+
+  const billListElements = await page.locator(
+    ".my-bill-wrapper-view.flex-column",
+  ).all();
+  const pageContent = await page.content();
+  await Deno.writeTextFile("bill_list.html", pageContent);
+
+  // List the bill for '本期账单'
+  await billListElements[1].tap();
+
+  // Wait for the bill listed
+  await sleep(1000);
+
+  await Deno.writeTextFile("after_bill_list.html", await page.content());
+
+  return page.locator(".my-bill-wrapper-view.flex-column").nth(1);
 }
